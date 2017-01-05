25a363c48e5b9e95cb7f41dad0fd78a236b6fe26
selim <selim@wearkinetic.com>
2017-01-05 13:00:00 -0500

    traversal

--- traversal.js
+++ traversal.js
@@ -1,9 +1,9 @@
-import { Map, fromJS } from 'immutable'
+import { fromJS } from 'immutable'
 import { Graph } from './graph'
 
 const KEY_SEPARATOR = '::'
 
-const newPath(starts) = {
+const newPath = (starts) => {
   let o = {}
   for(let start of starts){
     o[start] = []
@@ -11,10 +11,19 @@ const newPath(starts) = {
   return fromJS(o)
 }
 
+const newResult = (graph, starts) => starts
+  .filter((nodeKey) => graph.hasNode(nodeKey))
+  .reduce(
+    (acc, nodeKey) => graph.hasNode(nodeKey)
+      ? acc.set(nodeKey, graph.getNode(nodeKey))
+      : acc,
+    fromJS({})
+  )
+
 // renameKey
 const renameKey = (key) => {
   const splitted = key.split(KEY_SEPARATOR)
-  return splitted.length == 1
+  return splitted.length === 1
     ? {oldKey: splitted[0], newKey: splitted[0]}
     : {oldKey: splitted[0], newKey: splitted[1]}
 }
@@ -25,10 +34,10 @@ const Step = (nodeKey, edgeKey) => {
   }
 }
 
-const Trv = ({g, path = [], starts = []}) => {
+export const Trv = ({g, path = [], starts = []}) => {
 
-  let _graph = Graph(g)
-  let _result = fromJS({})
+  let _graph = g
+  let _result = newResult(_graph, starts)
   let _cache = fromJS({})
   let _path = (path.length > 0) ? path : newPath(starts)
   let _trvs = fromJS({})
@@ -53,7 +62,7 @@ const Trv = ({g, path = [], starts = []}) => {
       return false
     }
 
-    if(_trvs.size == 0){
+    if(_trvs.size === 0){
       return false
     }
 
@@ -66,9 +75,9 @@ const Trv = ({g, path = [], starts = []}) => {
   }
 
   // shallowSave
-  const shallowSave = (... keys){
+  function shallowSave(...keys) {
     if(_isDeep){
-      _trvs = _trvs.map((nestedTrv) => nestedTrv.shallowSave(keys...))
+      _trvs = _trvs.map((nestedTrv) => nestedTrv.shallowSave(...keys))
       return this
     }
 
@@ -87,7 +96,7 @@ const Trv = ({g, path = [], starts = []}) => {
   }
 
   // deepSave
-  const deepSave = (name) => {
+  function deepSave(name) {
     if(!_isDeep){
       return this
     }
@@ -105,7 +114,7 @@ const Trv = ({g, path = [], starts = []}) => {
   }
 
   // deepen
-  const deepen = () => {
+  function deepen() {
     if(_isDeep){
       _trvs = _trvs.map((nestedTrv) => nestedTrv.deepen())
       return this
@@ -114,7 +123,7 @@ const Trv = ({g, path = [], starts = []}) => {
     const trvs = {}
     _result.forEach((node) => {
       const nodeKey = node.get('key')
-      trvs[nodeKey] = Trv({g: _graph, path: _path, [nodeKey]})
+      trvs[nodeKey] = Trv({g: _graph, path: _path, starts: [nodeKey]})
     })
 
     _trvs = fromJS(trvs)
@@ -124,7 +133,7 @@ const Trv = ({g, path = [], starts = []}) => {
   }
 
   // flatten
-  const flatten = () => {
+  function flatten() {
     if(!_isDeep){
       return this
     }
@@ -147,9 +156,104 @@ const Trv = ({g, path = [], starts = []}) => {
     return this
   }
 
+  // shallowFilter
+  function shallowFilter(predicate){
+
+    if (_isDeep){
+      _trvs = _trvs.map((nestedTrv) => nestedTrv.shallowFilter(predicate))
+      return this
+    }
+
+    _result = _result.filter((node, nodeKey) => predicate(node, _path.get(nodeKey)))
+    return this
+
+  }
+
+  // deepFilter
+  function deepFilter(keepQuery){
+
+    if(!_isDeep){
+      return this
+    }
+
+    if(isVeryDeep()){
+      _trvs = _trvs.map((nestedTrv) => nestedTrv.deepFilter(keepQuery))
+      return this
+    }
+
+    let nodesToDiscard = []
+    _trvs.forEach((nestedTrv, nodeKey) => {
+      if(!keepQuery(nestedTrv, _path.get(nodeKey))){
+        nodesToDiscard.push(nodeKey)
+      }
+    })
+
+    nodesToDiscard.forEach((nodeKey) => {
+      _result = _result.delete(nodeKey)
+      _trvs = _trvs.delete(nodeKey)
+    })
+
+    return this
+  }
+
+  // hop
+  function hop(getIncomingNodes, label, rememberPath){
+
+    if(_isDeep){
+      _trvs = _trvs.map((nestedTrv) => nestedTrv.hop(getIncomingNodes, label, rememberPath))
+      return this
+    }
+
+    let newResult = fromJS({})
+    let newPath = fromJS({})
+
+    _result.forEach((aNode, aNodeKey) => {
+
+      const edges = getIncomingNodes
+        ? _graph.inEKeys(aNodeKey, label)
+        : _graph.outEKeys(aNodeKey, label)
+
+      edges.forEach((edgeLabel, edgeKey) => {
+        const bNode = _graph.hop(edgeKey, aNodeKey)
+        if(bNode === undefined){
+          return
+        }
+
+        const bNodeKey = bNode.get('key')
+        newResult = newResult.set(bNodeKey, bNode)
+        newPath = newPath.set(
+          bNodeKey,
+          rememberPath
+            ? _path.get(aNodeKey).concat([Step(aNodeKey, edgeKey)])
+            : _path.get(aNodeKey)
+        )
+      })
+
+    })
+
+    _result = newResult
+    _path = newPath
+
+    return this
+  }
+
+  // inV
+  function inV(label, rememberPath){
+    hop(true, label, rememberPath)
+    return this
+  }
+
+  // outV
+  function outV(label, rememberPath){
+    hop(false, label, rememberPath)
+    return this
+  }
+
   // public api
   return {
-    size, result, cache
+    isDeep, size, result, cache, isVeryDeep,
+    shallowSave, deepSave, deepen, flatten,
+    shallowFilter, deepFilter, hop, inV, outV
   }
 
 }
